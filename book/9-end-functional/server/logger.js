238a5e43fbab90489d64995e985f4a1bf45edcc0
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 07:10:46 +0300

    Update logger.js
    Added comments

--- book/9-end-functional/server/logger.js
+++ book/9-end-functional/server/logger.js
@@ -1,10 +1,14 @@
-
 const winston = require('winston');
 
+// Create a logger instance
 const logger = winston.createLogger({
+  // Set log level based on environment
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  // Define log message format
   format: winston.format.combine(winston.format.splat(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
+  // Specify the transports (where logs should be outputted)
+  transports: [new winston.transports.Console()], // Output logs to console
 });
 
+// Export the logger instance for use in other modules
 module.exports = logger;
