ba3f9fe9d7669467a4a05a9156b5151724b04220
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:36:19 +0300

    Update getRootUrl.js
    Added comments

--- book/9-end-functional/lib/api/getRootUrl.js
+++ book/9-end-functional/lib/api/getRootUrl.js
@@ -1,13 +1,16 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-multiple-empty-lines */
 
+// Function to get the root URL based on environment
 function getRootUrl() {
+  // Determine if in development or production environment
   const dev = process.env.NODE_ENV !== 'production';
+  // Define the root URL based on the environment
   const ROOT_URL = dev
-    ? process.env.NEXT_PUBLIC_URL_APP
-    : process.env.NEXT_PUBLIC_PRODUCTION_URL_APP;
+    ? process.env.NEXT_PUBLIC_URL_APP // Development URL
+    : process.env.NEXT_PUBLIC_PRODUCTION_URL_APP; // Production URL
 
-  return ROOT_URL;
+  return ROOT_URL; // Return the determined root URL
 }
 
-module.exports = getRootUrl;
+module.exports = getRootUrl; // Export the getRootUrl function
