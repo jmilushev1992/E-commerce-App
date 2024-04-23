5afdd959bb47e692618d5220cfd19b69372084ac
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:36:52 +0300

    Update public.js
    Added comments

--- book/9-end-functional/lib/api/public.js
+++ book/9-end-functional/lib/api/public.js
@@ -1,14 +1,14 @@
-
-
 import sendRequest from './sendRequest';
 
 const BASE_PATH = '/api/v1/public';
 
+// API method to fetch book details
 export const getBookDetailApiMethod = ({ slug }) =>
   sendRequest(`${BASE_PATH}/books/${slug}`, {
     method: 'GET',
   });
 
+// API method to fetch chapter details
 export const getChapterDetailApiMethod = ({ bookSlug, chapterSlug }, options = {}) =>
   sendRequest(`${BASE_PATH}/get-chapter-detail?bookSlug=${bookSlug}&chapterSlug=${chapterSlug}`, {
     method: 'GET',
