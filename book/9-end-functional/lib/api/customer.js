d2ba1ded9c8eec9f1b4c2e1bbd7f04b559626934
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:35:18 +0300

    Update customer.js

--- book/9-end-functional/lib/api/customer.js
+++ book/9-end-functional/lib/api/customer.js
@@ -3,13 +3,16 @@
 
 import sendRequest from './sendRequest';
 
+// Base path for customer API
 const BASE_PATH = '/api/v1/customer';
 
+// Method to fetch checkout session
 export const fetchCheckoutSessionApiMethod = ({ bookId, redirectUrl }) =>
   sendRequest(`${BASE_PATH}/stripe/fetch-checkout-session`, {
     body: JSON.stringify({ bookId, redirectUrl }),
   });
 
+// Method to get the list of my books
 export const getMyBookListApiMethod = (options = {}) =>
   sendRequest(`${BASE_PATH}/my-books`, {
     method: 'GET',
