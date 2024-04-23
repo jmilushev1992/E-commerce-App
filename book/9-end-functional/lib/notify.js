00ada3f11167fe6f16e522452ec324ee517c53f9
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:38:15 +0300

    Update notify.js
    Added comments

--- book/9-end-functional/lib/notify.js
+++ book/9-end-functional/lib/notify.js
@@ -1,8 +1,12 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-multiple-empty-lines */
-
 import { openSnackbarExported } from '../components/Notifier';
 
+/**
+ * Notifies the user by displaying a snackbar message.
+ * @param {object|string} obj - The object containing the message or a string message directly.
+ */
 export default function notify(obj) {
-  openSnackbarExported({ message: obj.message || obj.toString() });
+  // Extract the message from the object or convert object to string
+  const message = obj.message || obj.toString();
+  // Call the openSnackbarExported function to display the snackbar with the message
+  openSnackbarExported({ message });
 }
