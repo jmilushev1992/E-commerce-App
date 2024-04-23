4df7a77b05eb2ca8b986ebcab9e4d3a992bab2f4
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:53:42 +0300

    Update index.js
    Added comments

--- book/9-end-functional/server/api/index.js
+++ book/9-end-functional/server/api/index.js
@@ -1,12 +1,17 @@
 /* eslint-disable linebreak-style */
 
+// Importing API routes for different user roles
 const publicApi = require('./public');
 const customerApi = require('./customer');
 const adminApi = require('./admin');
 
+// Function to set up API routes
 function api(server) {
+  // Set up routes for public endpoints
   server.use('/api/v1/public', publicApi);
+  // Set up routes for customer endpoints
   server.use('/api/v1/customer', customerApi);
+  // Set up routes for admin endpoints
   server.use('/api/v1/admin', adminApi);
 }
 
