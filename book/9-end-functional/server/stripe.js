7eb0f05625ac2ad6c9abf02982ec99dab490019e
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 07:13:52 +0300

    Update stripe.js
    Added comments

--- book/9-end-functional/server/stripe.js
+++ book/9-end-functional/server/stripe.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-multiple-empty-lines */
 
+// Import necessary modules
 const Stripe = require('stripe');
 const lodash = require('lodash');
 
@@ -10,15 +11,19 @@ const logger = require('./logger');
 
 const getRootUrl = require('../lib/api/getRootUrl');
 
+// Determine environment and set API key and root URL accordingly
 const dev = process.env.NODE_ENV !== 'production';
 const API_KEY = dev ? process.env.STRIPE_TEST_SECRETKEY : process.env.STRIPE_LIVE_SECRETKEY;
 const ROOT_URL = getRootUrl();
 
+// Create a Stripe instance with the API key
 const stripeInstance = new Stripe(API_KEY, { apiVersion: '2020-08-27' });
 
+// Function to get the price ID of a book based on its slug
 function getBookPriceId(bookSlug) {
   let priceId;
 
+  // Set price ID based on the book slug and environment
   if (bookSlug === 'demo-book') {
     priceId = dev
       ? process.env.STRIPE_TEST_DEMO_BOOK_PRICE_ID
@@ -34,8 +39,10 @@ function getBookPriceId(bookSlug) {
   return priceId;
 }
 
+// Function to create a checkout session
 function createSession({ userId, bookId, bookSlug, userEmail, redirectUrl }) {
   logger.info(userId, bookId, bookSlug, userEmail, redirectUrl);
+  // Create and return a checkout session object
   return stripeInstance.checkout.sessions.create({
     customer_email: userEmail,
     payment_method_types: ['card'],
@@ -47,18 +54,21 @@ function createSession({ userId, bookId, bookSlug, userEmail, redirectUrl }) {
   });
 }
 
+// Function to retrieve a session using its ID
 function retrieveSession({ sessionId }) {
   return stripeInstance.checkout.sessions.retrieve(sessionId, {
     expand: ['payment_intent', 'payment_intent.payment_method'],
   });
 }
 
+// Route handler for Stripe checkout completion callback
 function stripeCheckoutCallback({ server }) {
   server.get('/stripe/checkout-completed/:sessionId', async (req, res) => {
     const { sessionId } = req.params;
     const session = await retrieveSession({ sessionId });
 
     try {
+      // Check if session and metadata exist and contain necessary information
       if (
         !session ||
         !session.metadata ||
@@ -69,6 +79,7 @@ function stripeCheckoutCallback({ server }) {
         throw new Error('Wrong session.');
       }
 
+      // Find user and book associated with the session
       const user = await User.findById(
         session.metadata.userId,
         '_id email purchasedBookIds freeBookIds',
@@ -76,6 +87,7 @@ function stripeCheckoutCallback({ server }) {
 
       const book = await Book.findOne({ _id: session.metadata.bookId }, 'name slug price').lean();
 
+      // Throw errors if user or book not found
       if (!user) {
         throw new Error('User not found.');
       }
@@ -84,6 +96,7 @@ function stripeCheckoutCallback({ server }) {
         throw new Error('Book not found.');
       }
 
+      // Process payment if session mode is 'payment'
       if (session.mode === 'payment') {
         await Book.buy({
           book,
@@ -94,8 +107,10 @@ function stripeCheckoutCallback({ server }) {
         throw new Error('Wrong session.');
       }
 
+      // Redirect to the specified URL after successful checkout
       res.redirect(`${ROOT_URL}${session.metadata.redirectUrl}`);
     } catch (err) {
+      // Handle errors and redirect with error message
       logger.error(err);
       res.redirect(
         `${ROOT_URL}${session.metadata.redirectUrl}?error=${err.message || err.toString()}`,
@@ -104,5 +119,6 @@ function stripeCheckoutCallback({ server }) {
   });
 }
 
+// Export functions for creating checkout session and handling checkout completion callback
 exports.createSession = createSession;
 exports.stripeCheckoutCallback = stripeCheckoutCallback;
