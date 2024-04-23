8da1a6792d53bd5e5280217370d0027dafd7e067
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 07:07:13 +0300

    Update EmailTemplate.js
    Added comments

--- book/9-end-functional/server/models/EmailTemplate.js
+++ book/9-end-functional/server/models/EmailTemplate.js
@@ -1,11 +1,15 @@
 /* eslint-disable linebreak-style */
+// Disables eslint rule for line break style
+
 /* eslint-disable no-multiple-empty-lines */
+// Disables eslint rule for multiple empty lines
 
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const { Schema } = mongoose;
 
+// MongoDB Schema definition
 const mongoSchema = new Schema({
   name: {
     type: String,
@@ -22,8 +26,10 @@ const mongoSchema = new Schema({
   },
 });
 
+// EmailTemplate model creation
 const EmailTemplate = mongoose.model('EmailTemplate', mongoSchema);
 
+// Function to insert email templates
 async function insertTemplates() {
   const templates = [
     {
@@ -43,19 +49,25 @@ async function insertTemplates() {
     },
   ];
 
+  // Iterating through templates array
   for (const t of templates) { // eslint-disable-line
     const et = await EmailTemplate.findOne({ name: t.name }); // eslint-disable-line
 
+    // Normalizing message (removing extra spaces and newlines)
     const message = t.message.replace(/\n/g, '').replace(/[ ]+/g, ' ').trim();
 
+    // If template doesn't exist, create it
     if (!et) {
       EmailTemplate.create({ ...t, message });
-    } else if (et.subject !== t.subject || et.message !== message) {
+    } 
+    // If template exists but subject or message has changed, update it
+    else if (et.subject !== t.subject || et.message !== message) {
       EmailTemplate.updateOne({ _id: et._id }, { $set: { message, subject: t.subject } }).exec();
     }
   }
 }
 
+// Function to get email template by name
 async function getEmailTemplate(name, params) {
   const et = await EmailTemplate.findOne({ name });
 
@@ -63,11 +75,13 @@ async function getEmailTemplate(name, params) {
     throw new Error(`No EmailTemplates found.`);
   }
 
+  // Compile template with parameters
   return {
     message: _.template(et.message)(params),
     subject: _.template(et.subject)(params),
   };
 }
 
+// Exporting functions
 exports.insertTemplates = insertTemplates;
 exports.getEmailTemplate = getEmailTemplate;
