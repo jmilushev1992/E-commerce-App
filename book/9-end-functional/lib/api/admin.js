1f823023fff4a1b84ef29aaa3b90737e1da26ec2
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:34:54 +0300

    Update admin.js
    Added comments

--- book/9-end-functional/lib/api/admin.js
+++ book/9-end-functional/lib/api/admin.js
@@ -3,18 +3,22 @@
 
 import sendRequest from './sendRequest';
 
+// Base path for admin API
 const BASE_PATH = '/api/v1/admin';
 
+// Method to get the list of books
 export const getBookListApiMethod = () =>
   sendRequest(`${BASE_PATH}/books`, {
     method: 'GET',
   });
 
+// Method to add a book
 export const addBookApiMethod = ({ name, price, githubRepo }) =>
   sendRequest(`${BASE_PATH}/books/add`, {
     body: JSON.stringify({ name, price, githubRepo }),
   });
 
+// Method to edit a book
 export const editBookApiMethod = ({ id, name, price, githubRepo }) =>
   sendRequest(`${BASE_PATH}/books/edit`, {
     body: JSON.stringify({
@@ -25,18 +29,21 @@ export const editBookApiMethod = ({ id, name, price, githubRepo }) =>
     }),
   });
 
+// Method to get details of a book
 export const getBookDetailApiMethod = ({ slug }) =>
   sendRequest(`${BASE_PATH}/books/detail/${slug}`, {
     method: 'GET',
   });
 
-// github methods
+// GitHub methods
 
+// Method to get GitHub repositories
 export const getGithubReposApiMethod = () =>
   sendRequest(`${BASE_PATH}/github/repos`, {
     method: 'GET',
   });
 
+// Method to sync book content
 export const syncBookContentApiMethod = ({ bookId }) =>
   sendRequest(`${BASE_PATH}/books/sync-content`, {
     body: JSON.stringify({ bookId }),
