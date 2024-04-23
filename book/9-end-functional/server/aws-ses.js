fc0e10ef13ba8631e189fca4eb97912b543ba64b
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 07:08:59 +0300

    Update aws-ses.js
    Added comments

--- book/9-end-functional/server/aws-ses.js
+++ book/9-end-functional/server/aws-ses.js
@@ -1,8 +1,8 @@
-
-
 const aws = require('aws-sdk');
 
+// Function to send email using AWS SES
 function sendEmail(options) {
+  // Create a new SES object
   const ses = new aws.SES({
     apiVersion: 'latest',
     region: process.env.AWS_REGION,
@@ -10,34 +10,37 @@ function sendEmail(options) {
     secretAccessKey: process.env.AWS_SECRETACCESSKEY,
   });
 
+  // Return a promise to send email
   return new Promise((resolve, reject) => {
     ses.sendEmail(
       {
-        Source: options.from,
+        Source: options.from, // Sender email address
         Destination: {
-          ToAddresses: options.to,
+          ToAddresses: options.to, // Recipient email address(es)
         },
         Message: {
           Subject: {
-            Data: options.subject,
+            Data: options.subject, // Email subject
           },
           Body: {
             Html: {
-              Data: options.body,
+              Data: options.body, // HTML content of the email
             },
           },
         },
-        ReplyToAddresses: options.replyTo,
+        ReplyToAddresses: options.replyTo, // Reply-to email address(es)
       },
+      // Callback function to handle the result of sending email
       (err, info) => {
         if (err) {
-          reject(err);
+          reject(err); // Reject the promise if there's an error
         } else {
-          resolve(info);
+          resolve(info); // Resolve the promise with the information if email is sent successfully
         }
       },
     );
   });
 }
 
+// Export the sendEmail function
 module.exports = sendEmail;
