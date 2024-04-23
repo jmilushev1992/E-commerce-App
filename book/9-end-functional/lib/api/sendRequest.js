bb702664fd9f96aecfb6772d62f5bb26c32e085a
Yulian Vasilev Milushev <84604915+jmilushev1992@users.noreply.github.com>
2024-04-23 06:37:26 +0300

    Update sendRequest.js
    Added comments

--- book/9-end-functional/lib/api/sendRequest.js
+++ book/9-end-functional/lib/api/sendRequest.js
@@ -1,12 +1,17 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-multiple-empty-lines */
-
 import 'isomorphic-unfetch';
 import getRootUrl from './getRootUrl';
 
+/**
+ * Sends a request to the server using fetch API.
+ * @param {string} path - The path of the API endpoint.
+ * @param {object} options - Additional options for the fetch request.
+ * @returns {Promise} - A Promise containing the response data.
+ */
 export default async function sendRequest(path, options = {}) {
+  // Set headers including Content-type
   const headers = { ...(options.headers || {}), 'Content-type': 'application/json; charset=UTF-8' };
 
+  // Fetch data from the server
   const response = await fetch(`${getRootUrl()}${path}`, {
     method: 'POST',
     credentials: 'same-origin',
@@ -14,8 +19,10 @@ export default async function sendRequest(path, options = {}) {
     headers,
   });
 
+  // Parse response JSON
   const data = await response.json();
 
+  // Throw error if response contains an error message
   if (data.error) {
     throw new Error(data.error);
   }
